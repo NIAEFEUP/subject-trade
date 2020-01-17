080c8f02aea1b16b973b51590bcf605d27652c63
tiago-falves <tiago.falves98@gmail.com>
2020-01-17 18:06:34 +0100

    subjects, targets and give ins are all stored in dictionaries

--- src/state.py
+++ src/state.py
@@ -5,6 +5,7 @@ class State:
     def add_student(self, student):
         self.students.append(student)
 
+    #Given two students and a subject, trades their classes for that given subject
     def trade_classes(self,student1,student2,subject_name):
         student1_class = student1.get_class_for_subject(subject_name)
         student2_class = student2.get_class_for_subject(subject_name)
@@ -13,13 +14,14 @@ class State:
 
     def generate_neighbour(self):
         for student in self.students: # select a student
-            for target_subject in student.subject_targets: # select a subject that has target classes (classes that the student wants to go to)
 
-                subject_name = target_subject[0]
+
+            for subject_name,target_classes in student.subject_targets.items(): # select a subject name and its respective  target classes
+
                 student_class = student.get_class_for_subject(subject_name) # get the current class of the first student for that subject
 
 
-                for target_class in target_subject[1]: # select, for that subject, a possible target class
+                for target_class in target_classes: # select, for that subject, a possible target class
         
 
                     for trader_student in self.students: # find a trader student
@@ -30,9 +32,10 @@ class State:
 
                         if trader_class is None or trader_class != target_class: # if target student does not have the desired class for that subject, continue
                             continue
-
+                  
 
                         trader_targets = trader_student.get_targets_for_subject(subject_name) # get the targets for that subject, for the trader student
+                        
                         if trader_targets is not None and student_class in trader_targets:
                             self.trade_classes(student,trader_student,subject_name) #trades the students classes for that subject
                             break
@@ -60,54 +63,61 @@ class Student:
         self.subject_targets = subject_targets
         self.subject_give_ins = subject_give_ins
     
-    def add_subject_and_class(self, subject_name, class_name):
-        self.subjects_and_classes.append([subject_name, class_name])
-
+    def add_subject_and_class(self, subject_name, class_number):
+        self.subjects_and_classes[subject_name] = class_number
 
     def add_subject_target(self, subject_name, class_number):
-        flag = False
-        for st in self.subject_targets:
-            if st[0] == subject_name:
-                st[1].append(class_number)
-                flag = True
-        if not flag: self.subject_targets.append([subject_name, [class_number]])
+        self.subject_targets[subject_name].append(class_number)
+
+        # flag = False
+        # for st in self.subject_targets:
+        #     if st[0] == subject_name:
+        #         st[1].append(class_number)
+        #         flag = True
+        # if not flag: self.subject_targets.append([subject_name, [class_number]])
 
     def remove_subject_target(self,subject_name):
-        for subject_target in self.subject_targets:
-            if subject_target[0] == subject_name:
-                self.subject_targets.remove(subject_target)
+        del self.subject_targets[subject_name]
+        # for subject_target in self.subject_targets:
+        #     if subject_target[0] == subject_name:
+        #         self.subject_targets.remove(subject_target)
 
     def add_subject_give_in(self, subject_name, class_number):
-        flag = False
-        for sgi in self.subject_give_ins:
-            if sgi[0] == subject_name:
-                sgi[1].append(class_number)
-                flag = True
-        if not flag: self.subject_give_ins.append([subject_name, [class_number]])
+        self.subject_give_ins[subject_name].append(class_number)
+        # flag = False
+        # for sgi in self.subject_give_ins:
+        #     if sgi[0] == subject_name:
+        #         sgi[1].append(class_number)
+        #         flag = True
+        # if not flag: self.subject_give_ins.append([subject_name, [class_number]])
 
 
     def get_class_for_subject(self, subject):
-        for subject_and_class in self.subjects_and_classes:
-            if subject_and_class[0] == subject:
-                return subject_and_class[1]
+        if subject in self.subjects_and_classes :
+            return self.subjects_and_classes[subject]
         return None
 
+
     def set_class_for_subject(self, subject, subject_class):
-        for i, subject_and_class in enumerate(self.subjects_and_classes):
-            if subject_and_class[0] == subject:
-                self.subjects_and_classes[i][1] = subject_class
-                return
+        if subject in self.subjects_and_classes:
+            self.subjects_and_classes[subject] = subject_class
+
 
     def get_targets_for_subject(self, subject):
-        for subject_and_targets in self.subject_targets:
-            if subject_and_targets[0] == subject:
-                return subject_and_targets[1]
+        if subject in self.subject_targets:
+            return self.subject_targets[subject]
+        # for subject_and_targets in self.subject_targets:
+        #     if subject_and_targets[0] == subject:
+        #         return subject_and_targets[1]
         return None
 
     def get_giveins_for_subject(self, subject):
-        for subject_and_giveins in self.subject_give_ins:
-            if subject_and_giveins[0] == subject:
-                return subject_and_giveins[1]
+        if subject in self.subject_give_ins:
+            return self.subject_give_ins[subject]
+
+        # for subject_and_giveins in self.subject_give_ins:
+        #     if subject_and_giveins[0] == subject:
+        #         return subject_and_giveins[1]
         return None
 
 
@@ -120,16 +130,18 @@ class Student:
 #TESTING
 
 #First Student
-subjects_and_classes1 = [["LPOO",1],["TCOM",1],["MDIS",1],["PLOG",1]]
-subject_targets1 = [["LPOO",[2,3]],["TCOM",[4,5]]]
-subject_give_ins1 = [["MDIS",[2,3]],["PLOG",[4,5]]] 
+subjects_and_classes1 = {"LPOO": 1,"TCOM": 1,"MDIS": 1,"PLOG": 1}
+subject_targets1 = {"LPOO": [2,3],"TCOM": [4,5]}
+subject_give_ins1 = {"MDIS": [2,3],"PLOG": [4,5]}
+
+
 a = Student(201603820,subjects_and_classes1,subject_targets1,subject_give_ins1)
 
 
 #Second student
-subjects_and_classes2 = [["LPOO",2],["TCOM",4],["MDIS",2],["PLOG",4]]
-subject_targets2 = [["LPOO",[1,4]],["TCOM",[1,5]]]
-subject_give_ins2 = [["MDIS",[1,3]],["PLOG",[1,5]]] 
+subjects_and_classes2 = {"LPOO": 2,"TCOM": 4,"MDIS": 2,"PLOG": 4}
+subject_targets2 = {"LPOO": [1,4], "TCOM": [1,5]}
+subject_give_ins2 = {"MDIS": [1,3],"PLOG": [1,5]}
 b = Student(201703820,subjects_and_classes2,subject_targets2,subject_give_ins2)
 
 #Creates the state
@@ -150,5 +162,8 @@ print("\nID: ",a.student_id,"\nClasses: ",a.subjects_and_classes,"\nTarget Class
 
 #Second Student after trading
 print("\nID: ",b.student_id,"\nClasses",b.subjects_and_classes,"\nTarget Classes",b.subject_targets)
+
+
+
 
 
