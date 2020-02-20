49ead42899fcf169a7721303ccb2ab7e635173f3
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2020-02-20 13:18:11 +0000

    refactor

--- src/generator.py
+++ src/generator.py
@@ -14,31 +14,32 @@ def generator_students(n_students, subjects):
         number_classes = random.randint(4,7)
 
         #subject classes
-        for _ in range(number_classes):                                                          
+        for i in range(0, number_classes):                                                          
             subject = random.choice(subjects)
             if subject not in subject_classes.keys():                                               #the subject can't be repeated
                 subject_classes[subject] = random.randint(1,13)
+            else: i -= 1
 
         #target classes
         for _ in range(random.randint(1,len(subject_classes))): 
             subject = random.choice(list(subject_classes.keys()))                                   #choose a class to change
             target = [random.randint(1,13) for _ in range(random.randint(1,5))]                     #class target number
-            target = list(set(filter(lambda x: x!= subject_classes[subject], target)))              #the target can't be in subject_classes 
+            target = list(set(filter(lambda x: x != subject_classes[subject], target)))              #the target can't be in subject_classes 
             subject_target[subject] = target            
 
-        #givin classes
-        for _ in range(random.randint(0,len(subject_classes) - len(subject_target))):               #the givin can't be a target
+        #give_in classes
+        for _ in range(random.randint(0,len(subject_classes) - len(subject_target))):               #the give_in can't be a target
             subject = random.choice(list(subject_classes.keys()))   
             while(subject in list(subject_target.keys())):                                  
                 subject = random.choice(list(subject_classes.keys()))
             
-            givin = []
-            while (givin == list()):                                                                #if there's a givin, the list can't be empty
-                givin = [random.randint(1,13) for _ in range(random.randint(1,5))]                  #givin classes 
-                givin = list(set(filter(lambda x: x!=subject_classes[subject], givin)))             #the givin can't be the actual class
+            give_in = []
+            while (give_in == list()):                                                                #if there's a give_in, the list can't be empty
+                give_in = [random.randint(1,13) for _ in range(random.randint(1,5))]                  #give_in classes 
+                give_in = list(set(filter(lambda x: x!=subject_classes[subject], give_in)))             #the give_in can't be the actual class
         
             
-            subject_give_in[subject] = givin 
+            subject_give_in[subject] = give_in 
 
 
 
@@ -61,14 +62,14 @@ def generator_students(n_students, subjects):
         print("STUDENT", s.student_id)
         print("CLASSES", subject_classes)
         print("TARGET", subject_target)
-        print("GIVIN", subject_give_in)
+        print("GIVE_IN", subject_give_in)
         print("BUDDIES",s.buddies)
         generated_students.append(s) 
     
     return generated_students 
 
 
-# Function that generates the schedule for classes givin the list of classes and the state
+# Function that generates the schedule for each classes given the list of classes
 def time_generator(subjects, state): 
     for number, subject in enumerate(subjects):
         for j in range(14): 
@@ -78,5 +79,5 @@ def time_generator(subjects, state):
 
 state = State()
 time_generator(['TCOM', 'FIS', 'LCOM', 'MPCP', 'FIS2', 'CMAT', 'AMAT', 'BDAD'], state)
-generator_students(50,['TCOM', 'FIS', 'LCOM', 'MPCP', 'FIS2', 'CMAT', 'AMAT', 'BDAD'])
+generator_students(1,['TCOM', 'FIS', 'LCOM', 'MPCP', 'FIS2', 'CMAT', 'AMAT', 'BDAD'])
 
