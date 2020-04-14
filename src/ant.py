fc47a1e7f8b6629a90c80f674f2958f76ddf6d62
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2020-04-14 17:00:32 +0100

    fixed chooses path

--- src/ant.py
+++ src/ant.py
@@ -1,10 +1,12 @@
 import random
 
+from node import Node
+
 class Ant:
-    def __init__(self, alpha, beta, node, graph):
+    def __init__(self, alpha, beta, current_node, graph):
         self.alpha = alpha
         self.beta = beta
-        self.node = node
+        self.current_node = current_node
         self.edges_path = []
         self.visited_nodes = []
         self.deposit_level = 2
@@ -13,7 +15,10 @@ class Ant:
 
     def chooses_path(self, current_node):
         s = current_node.get_sum(self.alpha, self.beta)
-        rand_val = random.randint(1, s)
+
+        if s - 1 > 0:
+            rand_val = random.randint(1, s)
+        else: rand_val = 0
         
         edges = []
         for _, b in current_node.edges.items():
@@ -43,9 +48,8 @@ class Ant:
                 
                 return (other_node, edges[i])
 
-
-    def update_node(self, node):
-        self.node = node
+    def update_node(self, new_node):
+        self.current_node = new_node
 
     def deposit_pheromones(self):
         for _, node in self.graph.nodes.items():
@@ -62,3 +66,8 @@ class Ant:
         else:
             a = self.graph.id / 200
             p = 1 / (1-a)
+
+    def expand_node(self):
+        node = Node(self.current_node.return_new_state(), self.graph.get_id())
+
+        self.graph.add_node(self.current_node, node)
