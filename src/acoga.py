97b5ab63e43f18913e20f4025e256167bba98c09
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2020-06-15 23:58:33 +0100

    New generator function

--- src/acoga.py
+++ src/acoga.py
@@ -23,21 +23,38 @@ class ACOGA:
         print('Genetic Algorithm time:', t2 - t1)
         return ret
 
+# # # # # Test what happens with diff number of students
+
+# list_of_states = DataBank.get_states_with_different_number()
+
+# time_ant_colony = 0
+# time_genetic_algorithm = 0
+# time_acoga = 0
+
+# for i, s in enumerate(list_of_states):
+#     print('Initial state heuristic:', s.get_score(), '\n')
+
+#     print('Ant colony with', str((i+1) * 2), 'students')
+#     t1 = time()
+#     print('Heuristic of:', AntColony.ant_colony(s)[0].heuristic)
+#     t2 = time()
+#     time_acoga += t2 - t1
+#     print('Took', t2 - t1, 'seconds\n\n')
 
 # # # # # Test what happens with more iterations
 
-root_state = DataBank.get_state_2()
+# root_state = DataBank.get_state_2()
 
-list_of_iterations = [500, 1000, 2000, 5000, 10000, 20000]
+# list_of_iterations = [500, 1000, 2000, 5000, 10000, 20000]
 
-for iterations in list_of_iterations:
-    t1 = time()
-    print('Heuristic value', ACOGA.acoga(root_state, iterations)[0].heuristic)
-    t2 = time()
-    print('Total time:', t2 - t1, '\n\n')
+# for iterations in list_of_iterations:
+#     t1 = time()
+#     print('Heuristic value', ACOGA.acoga(root_state, iterations)[0].heuristic)
+#     t2 = time()
+#     print('Total time:', t2 - t1, '\n\n')
 
 
-# # # # # Test mean time and heuristic values
+# # # # # Test how different number of students affects.
 
 # list_of_states = DataBank.get_states_with_different_number()
 
@@ -55,55 +72,55 @@ for iterations in list_of_iterations:
 #     time_acoga += t2 - t1
 #     print('Took', t2 - t1, 'seconds\n\n')
 
-# # # # # Test mean time and heuristic values
+# # # # Test mean time and heuristic values
 
-# root_state = DataBank.get_state_2()
+root_state = GenerateState.get_great_state(['A','B','C'], 200)
+root_state.get_score()
 
-# mean_heuristic_ant_colony = 0
-# mean_heuristic_genetic_algorithm = 0
-# mean_heuristic_acoga = 0
+print('Initial state score', root_state.heuristic)
 
-# mean_time_ant_colony = 0
-# mean_time_genetic_algorithm = 0
-# mean_time_acoga = 0
 
-# for i in range(10):
-#     print('Ant Colony: ' + str(i))
-#     t1 = time()
-#     mean_heuristic_ant_colony += AntColony.ant_colony(root_state)[0].heuristic
-#     t2 = time()
-#     mean_time_ant_colony += t2 - t1
+mean_heuristic_ant_colony = 0
+mean_heuristic_genetic_algorithm = 0
+mean_heuristic_acoga = 0
+
+mean_time_ant_colony = 0
+mean_time_genetic_algorithm = 0
+mean_time_acoga = 0
+
+N_ITERATIONS = 1
+
+for i in range(N_ITERATIONS):
+    print('Acoga: ' + str(i))
+    t1 = time()
+    mean_heuristic_acoga += ACOGA.acoga(root_state)[0].heuristic
+    t2 = time()
+    mean_time_acoga += t2 - t1
     
-#     print('Genetic Algorithm: ' + str(i))
-#     t1 = time()
-#     pop = GeneticAlgorithm.random_initial_population(root_state)
-#     mean_heuristic_genetic_algorithm += GeneticAlgorithm.genetic_algorithm(pop)[0].heuristic
-#     t2 = time()
-#     mean_time_genetic_algorithm += t2 - t1
+    print('Genetic Algorithm: ' + str(i))
+    t1 = time()
+    pop = GeneticAlgorithm.random_initial_population(root_state)
+    mean_heuristic_genetic_algorithm += GeneticAlgorithm.genetic_algorithm(pop)[0].heuristic
+    t2 = time()
+    mean_time_genetic_algorithm += t2 - t1
 
-#     print('Acoga: ' + str(i))
-#     t1 = time()
-#     mean_heuristic_acoga = ACOGA.acoga(root_state)[0].heuristic
-#     t2 = time()
-#     mean_time_acoga += t2 - t1
+mean_heuristic_ant_colony = mean_heuristic_ant_colony/N_ITERATIONS
+mean_heuristic_genetic_algorithm = mean_heuristic_genetic_algorithm/N_ITERATIONS
+mean_heuristic_acoga = mean_heuristic_acoga/N_ITERATIONS
 
-# mean_heuristic_ant_colony = mean_heuristic_ant_colony/10
-# mean_heuristic_genetic_algorithm = mean_heuristic_genetic_algorithm/10
-# mean_heuristic_acoga = mean_heuristic_acoga/10
-
-# mean_time_ant_colony = mean_time_ant_colony/10
-# mean_time_genetic_algorithm = mean_time_genetic_algorithm/10
-# mean_time_acoga = mean_time_acoga/10
+mean_time_ant_colony = mean_time_ant_colony/N_ITERATIONS
+mean_time_genetic_algorithm = mean_time_genetic_algorithm/N_ITERATIONS
+mean_time_acoga = mean_time_acoga/N_ITERATIONS
 
 
-# print('\n\n\n')
-# print('Ant Colony mean time: ' + str(mean_time_ant_colony))
-# print('Ant Colony mean heuristic: ' + str(mean_heuristic_ant_colony))
+print('\n\n\n')
+print('Ant Colony mean time: ' + str(mean_time_ant_colony))
+print('Ant Colony mean heuristic: ' + str(mean_heuristic_ant_colony))
 
-# print('\n')
-# print('Genetic Algorithm mean time: ' + str(mean_time_genetic_algorithm))
-# print('Genetic Algorithm mean heuristic: ' + str(mean_heuristic_genetic_algorithm))
+print('\n')
+print('Genetic Algorithm mean time: ' + str(mean_time_genetic_algorithm))
+print('Genetic Algorithm mean heuristic: ' + str(mean_heuristic_genetic_algorithm))
 
-# print('\n')
-# print('Acoga mean time: ' + str(mean_time_acoga))
-# print('Acoga mean heuristic: ' + str(mean_heuristic_acoga))
+print('\n')
+print('Acoga mean time: ' + str(mean_time_acoga))
+print('Acoga mean heuristic: ' + str(mean_heuristic_acoga))
--- src/generator.py
+++ src/generator.py
@@ -2,9 +2,20 @@ from student import Student
 from hour import Hour 
 from state import State 
 
-import random
+from random import sample, choice, randint
 
-MAXIMUM_CLASSES = 7
+MAXIMUM_CLASSES = 6
+NUMBER_OF_CLASSES = 12
+
+class NewDict:
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+
+    def __repr__(self):
+        return 'NewD' + self.dictionary.__repr__()
+        
+    def items(self):
+        return self.dictionary.items()
 
 class GenerateState:
     # Function that generates students objects
@@ -15,86 +26,158 @@ class GenerateState:
             subject_classes ={}
             subject_target = {}
             subject_give_in = {}
-            number_classes = random.randint(4,6)
+            number_classes = randint(4,NUMBER_OF_CLASSES)
 
             #subject classes
             for j in range(0, number_classes):                                                          
-                subject = random.choice(subjects)
+                subject = choice(subjects)
                 if subject not in subject_classes.keys():                                               #the subject can't be repeated
-                    subject_classes[subject] = random.randint(1,5)
+                    subject_classes[subject] = randint(1,NUMBER_OF_CLASSES)
                 else: j -= 1
 
             #target classes
-            for _ in range(random.randint(1,len(subject_classes))): 
-                subject = random.choice(list(subject_classes.keys()))                                   #choose a class to change
-                target = [random.randint(1, MAXIMUM_CLASSES) for _ in range(random.randint(1,5))]                     #class target number
+            for _ in range(randint(1,len(subject_classes))): 
+                subject = choice(list(subject_classes.keys()))                                   #choose a class to change
+                target = [randint(1, MAXIMUM_CLASSES) for _ in range(randint(1,NUMBER_OF_CLASSES))]                     #class target number
                 target = list(set(filter(lambda x: x != subject_classes[subject], target)))              #the target can't be in subject_classes 
                 subject_target[subject] = target            
 
             #give_in classes
-            for _ in range(random.randint(0,len(subject_classes) - len(subject_target))):               #the give_in can't be a target
-                subject = random.choice(list(subject_classes.keys()))   
+            for _ in range(randint(0,len(subject_classes) - len(subject_target))):               #the give_in can't be a target
+                subject = choice(list(subject_classes.keys()))   
                 while(subject in list(subject_target.keys())):                                  
-                    subject = random.choice(list(subject_classes.keys()))
+                    subject = choice(list(subject_classes.keys()))
                 
                 give_in = []
                 while (give_in == list()):                                                                #if there's a give_in, the list can't be empty
-                    give_in = [random.randint(1, MAXIMUM_CLASSES) for _ in range(random.randint(1,5))]                  #give_in classes 
+                    give_in = [randint(1, MAXIMUM_CLASSES) for _ in range(randint(1,NUMBER_OF_CLASSES))]                  #give_in classes 
                     give_in = list(set(filter(lambda x: x!=subject_classes[subject], give_in)))             #the give_in can't be the actual class
             
                 
                 subject_give_in[subject] = give_in 
 
-            s = Student(20180000+i, subject_classes, subject_target, subject_give_in, {}) 
-            for _ in range(random.randint(0,len(subject_classes))):                                     #number of subjects with buddies
+            s = Student(i, subject_classes, subject_target, subject_give_in, {})
+
+            for _ in range(randint(0,len(subject_classes))):                                     #number of subjects with buddies
                 buddies = []
                 for _ in range(1,4):                                                                    #number of buddies in a subject
-                    buddy = random.randint(20180000, (20180000+n_students-1))
+                    buddy = randint(0, (n_students-1))
                     while(buddy == s.student_id):                                                       #avoid the buddy being him self
-                        buddy = random.randint(20180000, (20180000+n_students-1))
+                        buddy = randint(0, (n_students-1))
                     buddies.append(buddy)
                     
-                subject = random.choice(list(subject_classes.keys()))
+                subject = choice(list(subject_classes.keys()))
                 while(subject in list(s.buddies.keys())):                                               #the subject cannot be already at s.buddies
-                    subject = random.choice(list(subject_classes.keys()))
+                    subject = choice(list(subject_classes.keys()))
 
                 s.add_buddies(subject,list(set(buddies)))
-
-            
-            # print("------------------------------------------")
-            # print("STUDENT", s.student_id)
-            # print("CLASSES", subject_classes)
-            # print("TARGET", subject_target)
-            # print("GIVE_IN", subject_give_in)
-            # print("BUDDIES",s.buddies)
             
             generated_students.append(s) 
         
         return generated_students 
 
 
+    @staticmethod
+    def great_students(list_of_subjects, number_students):
+        generated_students = []
+        dict_of_targets = {}
+        dict_of_student_info = {}
+
+        for i in range(number_students):
+            subject_target = {}
+            subject_give_in = {}
+
+            for el in sample(list_of_subjects, 1):
+                number_of_targets = randint(1,3)
+                # number_of_targets random targets from 1 to NUMBER_OF_CLASSES
+                subject_target[el] = sample([ j for j in range(1, NUMBER_OF_CLASSES + 1) ], number_of_targets)
+            
+            c = choice(list_of_subjects)
+            while c in subject_target.keys():
+                c = choice(list_of_subjects)
+            
+            number_of_give_ins = randint(1,3)
+            # number_of_give_ins random give ins from 1 to NUMBER_OF_CLASSES
+            subject_give_in[el] = sample([ j for j in range(1, NUMBER_OF_CLASSES + 1) ], number_of_give_ins)
+
+            # Fill dict of targets with a random target of this student
+            for subject, target in subject_target.items():
+                if subject not in dict_of_targets:
+                    dictionary = {choice(list(subject_target)): 1}
+                    dict_of_targets[subject] = NewDict(dictionary)
+                else:
+                    clas = choice(list(subject_target))
+                    if clas not in dict_of_targets[subject].dictionary:
+                        dict_of_targets[subject].dictionary[clas] = 1
+                    else:
+                        dict_of_targets[subject].dictionary[clas] += 1
+
+            dict_of_student_info[i] = [subject_target, subject_give_in]
+
+        for i in range(number_students):
+            subjects_and_classes = {}
+
+            for subject in list_of_subjects:
+                for clas, counter in dict_of_targets[subject].dictionary.items():
+                    if counter > 0:
+                        counter -= 1
+                        subjects_and_classes[subject] = clas
+                        break
+                subjects_and_classes[subject] = choice([j for j in range(1, NUMBER_OF_CLASSES + 1)])
+
+            subject_targets = dict_of_student_info[i][0]
+            subject_give_ins = dict_of_student_info[i][1]
+
+            generated_students.append(Student(i, subjects_and_classes, subject_targets, subject_give_ins, {}))
+
+            
+        return generated_students 
+
     # Function that generates the schedule for each classes given the list of classes
     @staticmethod
-    def time_generator(subjects, state): 
+    def random_time_generator(subjects, state): 
         days_of_the_week = {'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'}
         for number, subject in enumerate(subjects, start=1):
-            for j in range(5):
+            for j in range(NUMBER_OF_CLASSES):
                 hour_1 = Hour(7, 0) + Hour(j*2, 0)
                 hour_2 = hour_1 + Hour(2, 0)
-                state.add_schedule(subject, j, hour_1, hour_2, random.sample(days_of_the_week, 1)[0])
+                state.add_schedule(subject, j, hour_1, hour_2, sample(days_of_the_week, 1)[0])
+        return state 
+
+    @staticmethod
+    def time_generator(subjects, state): 
+        days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+        for number, subject in enumerate(subjects, start=1):
+            for j in range(NUMBER_OF_CLASSES):
+                hour_1 = Hour(7, 0) + Hour(j*2, 0)
+                hour_2 = hour_1 + Hour(2, 0)
+                state.add_schedule(subject, j, hour_1, hour_2, days_of_the_week[j % (len(days_of_the_week) - 1)])
         return state 
 
     @staticmethod
     def get_random_state(list_of_subjects, number_students):
         state = State()
-        GenerateState.time_generator(list_of_subjects, state)
+        GenerateState.random_time_generator(list_of_subjects, state)
         
         for student in GenerateState.generator_students(number_students, list_of_subjects):
             state.add_student(student)
 
         return state
+
+    @staticmethod
+    def get_great_state(list_of_subjects, number_students):
+        state = State()
+        GenerateState.time_generator(list_of_subjects, state)
+
+        for student in GenerateState.great_students(list_of_subjects, number_students):
+            state.add_student(student)
+
+        return state
+
+        
     
 # state = State()
-# GenerateState.time_generator(['TCOM', 'FIS', 'LCOM', 'MPCP', 'FIS2', 'CMAT', 'AMAT', 'BDAD'], state)
+# GenerateState.random_time_generator(['TCOM', 'FIS', 'LCOM', 'MPCP', 'FIS2', 'CMAT', 'AMAT', 'BDAD'], state)
 # GenerateState.generator_students(40,['TCOM', 'FIS', 'LCOM', 'MPCP', 'FIS2', 'CMAT', 'AMAT', 'BDAD'])
 
+GenerateState.get_great_state(['a','b','c'], 100)
